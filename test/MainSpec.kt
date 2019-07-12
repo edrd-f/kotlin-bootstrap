92fe8461f5355dfae87dcd843581354b4414f80f
Eduardo Fonseca <edrd.fons@gmail.com>
2019-07-11 23:49:00 -0300

    Reindent files to 4 spaces

--- test/MainSpec.kt
+++ test/MainSpec.kt
@@ -4,9 +4,9 @@ import org.spekframework.spek2.style.specification.describe
 import org.spekframework.spek2.Spek
 
 object MainSpec : Spek({
-   describe("main") {
-      it("tests nothing") {
-         assert(true)
-      }
-   }
+    describe("main") {
+        it("tests nothing") {
+            assert(true)
+        }
+    }
 })

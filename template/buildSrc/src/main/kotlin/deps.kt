da48b9a19b3bad91286a028c21f9ff0aaa72f8ec
Eduardo Fonseca <edrd.fons@gmail.com>
2021-06-26 23:14:00 -0300

    Centralize dependencies

---
+++ template/buildSrc/src/main/kotlin/deps.kt
@@ -0,0 +1,8 @@
+@file:Suppress("Unused", "ClassName")
+
+object deps {
+	const val junitApi = "org.junit.jupiter:junit-jupiter-api:$junitVersion"
+	const val junitEngine = "org.junit.jupiter:junit-jupiter-engine:$junitVersion"
+}
+
+private const val junitVersion = "5.7.2"
--- template/modules/application/application.gradle.kts
+++ template/modules/application/application.gradle.kts
@@ -11,6 +11,6 @@ repositories {
 application.mainClass.set("{{basePackage}}.MainKt")
 
 dependencies {
-	testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.7.2")
-	testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.7.2")
+	testImplementation(deps.junitApi)
+	testRuntimeOnly(deps.junitEngine)
 }
